868fa6dd5f7c6380b332767be49e17978cf2d08f
Tishana <tishana.trainor@gmail.com>
2023-04-28 11:51:57 -0400

    stuff an thangs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css"
+import { useState, useEffect } from "react"
+import TodoList from "./components/TodoList"
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([])
+console.log(todos)
+  useEffect(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos && savedTodos !== "undefined" && savedTodos !== "null") {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
+
+  const addTodo = (e) => {
+    const newTodo = { text: e.target.value, id: Date.now(), completed: false }
+    localStorage.setItem("todos", JSON.stringify([newTodo, ...todos]))
+    setTodos([newTodo, ...todos])
+    e.target.value = ""
+  }
+
+  const completeTodo = (id, e) => {
+    const todosCopy = [...todos]
+    const indexOfTodo = todosCopy.findIndex((i) => i.id === id)
+    todosCopy[indexOfTodo].completed = !todosCopy[indexOfTodo].completed
+    localStorage.setItem("todos", JSON.stringify([...todosCopy]))
+    setTodos([...todosCopy])
+  }
+
+  const editTodoText = (id, e) => {
+    const todosCopy = [...todos]
+    const indexOfTodo = todosCopy.findIndex((i) => i.id === id)
+    todosCopy[indexOfTodo].text = e.target.value
+    localStorage.setItem("todos", JSON.stringify([...todosCopy]))
+    setTodos([...todosCopy])
+    e.target.value = ""
+  }
+
+  const deleteTodo = (id) => {
+    const todosCopy = [...todos]
+    const indexOfTodo = todosCopy.findIndex((i) => i.id === id)
+    todosCopy.splice(indexOfTodo, 1)
+    localStorage.setItem(
+        "todos",
+        JSON.stringify([...todosCopy])
+      )
+    setTodos([...todosCopy])
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList
+        todos={todos}
+        addTodo={addTodo}
+        completeTodo={completeTodo}
+        editTodoText={editTodoText}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
-
-export default App;
